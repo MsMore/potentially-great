dc2562b57e47245faba88175283480f446049e5e
MsMore <68859625+MsMore@users.noreply.github.com>
2020-07-27 20:22:37 +0530

    Uploaded some of my work

---
+++ hackerrank/Array manipulation - riddhi.py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split())
+
+array = [0] * n
+
+#print(array)
+
+for i in range(0,m):
+    a,b,k=map(int,input().split())
+
+    for j in range(a-1,b):
+        
+        array[j] += k
+print(max(array))
